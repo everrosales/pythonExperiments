793e5d044123d9646275ff9bae0a478368d89028
Everardo Rosales <ever.rosales.3@gmail.com>
2014-07-31 01:01:30 -0400

    Refactoring and Comments
    Refactored the chain creation out of __init__ and into
    MarkovChain.randChain() and MarkovChain.createChain(). Also added
    documentation and more verbose help from Commandline.
    
    TODO:
    - Optimize storing/use of the word pair dictionary.
    - Add ability to output chain to a file.

--- markovChain/markovChain.py
+++ markovChain/markovChain.py
@@ -2,17 +2,24 @@ from random import randrange as rand
 import sys, getopt
 
 class MarkovChain:
+    """Basic class for generating Markov Chains from text files. """
     def __init__(self, textFile, isParagraph):
+        """Reads input text files and filters out Non Ascii characters. """
         text = open(textFile)
-        lines = text.readlines()
-        wordDict = {}
+        listTextLines = text.readlines()
         if isParagraph:
-            lines = [delNonAscii(line) for line in lines]
+            listTextLines = [self.filterText(line) for line in listTextLines]
         else:
-            lines = [delNonAscii(line.replace('.', '. [$STOP]')) for line in lines]
-        for lineIndex in range(len(lines)):
-            splitLine = lines[lineIndex].split()
-            if lineIndex < len(lines) - 1 and lines[lineIndex + 1] == ['\n']:
+            listTextLines = [self.filterText(line.replace('.', '. [$STOP]')) for line in listTextLines]
+        self.textLines = listTextLines
+        text.close()
+
+    def randChain(self):
+        """Generates Markov Chain uses stored textLines. Returns str."""
+        wordDict = {}
+        for lineIndex in range(len(self.textLines)):
+            splitLine = self.textLines[lineIndex].split()
+            if lineIndex < len(self.textLines) - 1 and self.textLines[lineIndex + 1] == ['\n']:
                 splitLine.append('[$STOP]')
             for wordIndex in range(len(splitLine)):
                 if wordIndex > 0 and wordIndex < len(splitLine) - 1:
@@ -22,45 +29,59 @@ class MarkovChain:
                         wordDict[splitLine[wordIndex - 1] + " " + splitLine[wordIndex]] += [splitLine[wordIndex + 1]] 
                     else:
                         wordDict[splitLine[wordIndex - 1] + " " + splitLine[wordIndex]] = [splitLine[wordIndex + 1]]
-        self.words = wordDict
-        text.close()
-        self.rand()
+        randWordPair = self.getStartingWord(wordDict)
+        return self.createChain(randWordPair, randWordPair, wordDict)
 
-    def rand(self):
-        randWordPair = self.getStartingWord()
-        return self.createChain(randWordPair, randWordPair)
-
-    def getStartingWord(self):
-        randWordPair = self.words.keys()[rand(0,len(self.words.keys()))]
+    def getStartingWord(self, wordDict):
+        """Randomly select a starting word and ensure that it is Capitalized. """
+        randWordPair = wordDict.keys()[rand(0,len(wordDict.keys()))]
         if randWordPair[0].isupper():
             return randWordPair
-        return self.getStartingWord()
+        return self.getStartingWord(wordDict)
 
-    def createChain(self, startWord, chain):
+    def createChain(self, startWord, chain, wordDict):
+        """Recussively creates chain until hitting a keyword ([$STOP})
+            chain str stored in MarkovChain.chain.
+            """
         if startWord == '[$STOP]':
             self.chain = chain
-        elif startWord in self.words:
-            newWord = self.words[startWord][rand(0,len(self.words[startWord]))]
+        elif startWord in wordDict:
+            newWord = wordDict[startWord][rand(0,len(wordDict[startWord]))]
             if newWord == '[$STOP]':
                 self.chain = chain
                 return
             chain += ' ' + newWord
-            return self.createChain(startWord.split()[1] + ' ' + newWord, chain)
+            return self.createChain(startWord.split()[1] + ' ' + newWord, chain, wordDict)
         else:
             self.chain = chain
 
+    def filterText(self, string):
+        """Filters Non Acsii characters. Returns str. """
+        return "".join(char for char in string if ord(char)<128)
+
     def __str__(self):
+        """Overrides __str__ to print chain stored in MarkovChain.chain. """
         return self.chain
 
-
-def delNonAscii(string):
-    return "".join(char for char in string if ord(char)<128)
-
-
 def main(argv):
+    """Random Markov Chain Generator
+
+        Description:
+            Takes an input file and returns either a sentence or
+            a paragraph of psuedo random text.
+            
+        Usage:
+            -f <fileName>   Specifies input file.
+            -h              Help command.
+            -p              Indicates whether to return a sentence
+                            or a paragraph, "-p" returns a paragraph.
+            
+            --input_file=<fileName>     Same as "-i", see above.
+            --help                      Same as "-h", see above.
+        """
     usage = 'Usage: markov.py --input_file=<targetFile> [Optional: -p]'
     try:
-        opts, args = getopt.getopt(argv, 'hf:p',['help=', 'input_file='])
+        opts, args = getopt.getopt(argv, 'hf:p',['help', 'input_file='])
     except getopt.GetoptError:
         print usage
         sys.exit(2)
@@ -68,7 +89,7 @@ def main(argv):
     targetFile = None
     for opt, arg in opts:
         if opt in ('-h', '--help'):
-            print usage
+            print help(main)
             sys.exit()
         elif opt in ('-f','--input_file'):
             targetFile = arg
@@ -78,6 +99,7 @@ def main(argv):
         print usage
         sys.exit(2)
     markov = MarkovChain(targetFile, isParagraph)
+    markov.randChain()
     print markov
     sys.exit()
 
