dad65630d8ad597859be5ac32ab6b4ee5e3de766
Everardo Rosales <ever.rosales.3@gmail.com>
2014-08-04 23:39:17 -0400

    Upper Triangular And Determinants
    Added support for finding a matrix's upper triangular form and
    determinant. Also enabled creating a populated matrix from __init__.

--- Matrix Support/matrix.py
+++ Matrix Support/matrix.py
@@ -1,17 +1,20 @@
 class Matrix:
 	"""Class that add Matrix Support in python. """
- 	def __init__(self, rows, columns):
+ 	def __init__(self, rows, columns, matrix= None):
  		self.columns = columns
  		self.rows = rows
- 		self.matrix = [[0 for x in range(columns)] for y in range(rows)]
+ 		if matrix != None:
+ 			self.matrix = matrix
+ 		else:
+ 			self.matrix = [[0 for x in range(columns)] for y in range(rows)]
 
  	def setItem(self, row, column, newValue):
  		self.matrix[row][column] = newValue
 
- 	def setRow(self, rowIndex, list, extendMatrix= False):
+ 	def setRow(self, rowIndex, list, fitMatrix= False):
  		if len(list) == self.columns:
  			self.matrix[rowIndex] = list
- 		elif extendMatrix:
+ 		elif fitMatrix:
  			diffLen = len(list) - self.columns
  			if diffLen > 0:
  				for row in self.matrix:
@@ -25,11 +28,11 @@ class Matrix:
  		else:
  			print "Row size does not fit the dimensions of the Matrix. "
 
- 	def setColumn(self, columnIndex, list, extendMatrix= False):
+ 	def setColumn(self, columnIndex, list, fitMatrix= False):
  		if len(list) == self.rows:
  			for index in range(len(list)):
  				self.matrix[index][columnIndex] = list[index]
- 		elif extendMatrix:
+ 		elif fitMatrix:
  			diffLen = len(list) - self.rows
  			if diffLen > 0:
  				for i in range(diffLen):
@@ -42,6 +45,45 @@ class Matrix:
  				self.matrix[index][columnIndex] = list[index]
  		else:
  			print "Column size does not fit the dimensions of the Matrix. "
+
+ 	def upperTriangular(self, storeUpperTri= False):
+ 		index = 0
+ 		newMatrix = self.matrix[:]
+ 		while index < self.columns:
+ 			newMatrix = self.reduceRow(index, newMatrix)
+ 			index += 1
+ 		return Matrix(len(newMatrix), len(newMatrix[0]), newMatrix)
+
+ 	def reduceRow(self, pivotRowIndex, currentMatrix):
+ 		for index in range(pivotRowIndex + 1, self.rows):
+ 			currentMatrix[index] = self.divideRow(pivotRowIndex, index, currentMatrix)
+ 		return currentMatrix
+
+ 	def divideRow(self, pivotRowIndex, targetRowIndex, matrix):
+ 		pivotRow = matrix[pivotRowIndex]
+ 		targetRow = matrix[targetRowIndex]
+ 		if targetRow[pivotRowIndex] != 0:
+ 			diff = float(targetRow[pivotRowIndex])/float(pivotRow[pivotRowIndex])
+ 		else:
+ 			return targetRow
+ 		dividedRow = []
+ 		for itemIndex in range(len(targetRow)):
+ 			newValue = round((targetRow[itemIndex] + pivotRow[itemIndex]*(-diff)), 5)
+ 			if newValue.is_integer():
+ 				newValue = int(newValue)
+ 			dividedRow.append(newValue)
+ 		return dividedRow
+
+ 	def determinant(self):
+ 		if self.columns == self.rows:
+ 			upperTri = self.upperTriangular()
+ 			det = 1
+ 			for index in range(self.rows):
+ 				det *= upperTri.matrix[index][index]
+ 			return det
+
+ 		else:
+ 			return "Matrix is not square. "
 
  	def __str__(self):
  		string = ''
--- Matrix Support/matrixTest.py
+++ Matrix Support/matrixTest.py
@@ -4,6 +4,10 @@ from matrix import Matrix
 class MatrixTests(unittest.TestCase):
 	def setUp(self):
 		self.matrix = Matrix(5,5)
+		self.matrix3by3 = Matrix(3,3)
+		self.matrix3by3.setRow(0, [1,2,3])
+		self.matrix3by3.setRow(1, [1,5,1])
+		self.matrix3by3.setRow(2, [1,0,2])
 
 	def testPrint(self):
 		testMatrix = [[0,0,0,0,0] for i in range(5)]
@@ -18,7 +22,6 @@ class MatrixTests(unittest.TestCase):
 		testMatrix = [[0,0,0,0,0,0] for i in range(5)]
 		testMatrix[1] = [1,1,1,1,1,0]
 		testMatrix[2] = [1,1,1,1,1,1]
-		print self.matrix
 		self.assertEqual(testMatrix, self.matrix.matrix)
 
 	def testSetColumn(self):
@@ -28,8 +31,19 @@ class MatrixTests(unittest.TestCase):
 		self.matrix.setColumn(2,[1,1,1,1,1,1], True)
 		testMatrix = [[0,1,1,0,0] for i in range(6)]
 		testMatrix[5][1] = 0
-		print self.matrix
 		self.assertEqual(testMatrix, self.matrix.matrix)
+
+	def testReduceRow(self):
+		newMatrix = self.matrix3by3.reduceRow(0, self.matrix3by3.matrix)
+		testMatrix = [[1, 2, 3], [0, 3, -2], [0, -2, -1]]
+		self.assertEqual(testMatrix, newMatrix)
+
+	def testUpperTriangular(self):
+		testMatrix = [[1, 2, 3], [0, 3, -2], [0, 0, -2.33333]]
+		self.assertEqual(testMatrix, self.matrix3by3.upperTriangular().matrix)
+	
+	def testDet(self):
+		self.assertEqual(-6.99999, self.matrix3by3.determinant())
 
 if __name__ == '__main__':
 	unittest.main()
